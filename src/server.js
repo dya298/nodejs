a33a6572f9d350e5828751bf733878bf2862cd06
duy <tranduy030700@gmail.com>
2022-09-06 23:36:52 +0700

    update struct files and babel-viewEngine

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 2999;
+
+configViewEngine(app);
+
+app.get("/", (req, res) => {
+  res.render(`index.ejs`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
